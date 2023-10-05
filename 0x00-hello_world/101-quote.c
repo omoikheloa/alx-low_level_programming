392de1da928ede115b0c5c12ee72718dac1401c0
Omo <omoikheloa56@gmail.com>
2023-10-05 14:56:36 -0700

    compiling using -Wall

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-/*
- * main - entry point
- *
- * Return: Always 1 (Success)
+/**
+ * main - write to stdout
+ * return: Always 1(success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar,2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

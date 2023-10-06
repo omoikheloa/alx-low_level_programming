8327706b4cbb7b927715f56c2f33bbc6f3d22537
Omo <omoikheloa56@gmail.com>
2023-10-06 11:09:36 -0700

    print a-z using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: print leters of the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char om = 'a';
+
+	while (om <= 'z')
+	{
+		putchar(om);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

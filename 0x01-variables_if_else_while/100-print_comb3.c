875356886e5ed6ee8f4ffa3b405d3fa78587cc13
Omo <omoikheloa56@gmail.com>
2023-10-06 16:31:46 -0700

    print double digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints all possible
+ * combinations of two digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	while (b < 10)
+	{
+		a = 0;
+		while (a < 10)
+		{
+			if (b != a && b < a)
+			{
+				putchar('0' + b);
+				putchar('0' + a);
+
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			a++;
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}

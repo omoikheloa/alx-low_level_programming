f0b6690e44536085b287ae7a6f4a56d238207c46
Omo <omoikheloa56@gmail.com>
2023-10-06 17:09:28 -0700

    final one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: create two pairs of numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a; b <= 99; b++)
+		{
+			if (b != a)
+			{
+				putchar(a / 10 + 48);
+				putchar(a % 10 + 48);
+				putchar(' ');
+				putchar(b / 10 + 48);
+				putchar(b % 10 + 48);
+
+				if (a * 100 + b  != 989)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+
+			}
+
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}

05e3fb08728440fdd0a3c6eebdebb631a47750bb
Omo <omoikheloa56@gmail.com>
2023-10-19 05:31:07 -0700

    corrections for strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,7 @@
 
 /**
  * cap_string - capitalize words on a string
+ * sep_words - Ascii values of various word seperator characters
  * @s: the input string
  *
  * Return: prints modified string
@@ -9,25 +10,24 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (s[i] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == '.')
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] >= 'a' && s[i] <= 'z')
+			if (*(s + count) == sep_words[i])
 			{
-				s[i] = s[i] - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		else
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-			{
-				s[i] = s[i] + 32;
-			}
-		}
-		i++;
+		count++;
 	}
 	return (s);
 }

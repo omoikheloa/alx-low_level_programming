fce177928b8f89831618332ed48fe0aae3188467
Omo <omoikheloa56@gmail.com>
2023-10-19 05:16:44 -0700

    capitalize certain strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize words on a string
+ * @s: the input string
+ *
+ * Return: prints modified string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == '.')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		else
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+			{
+				s[i] = s[i] + 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}

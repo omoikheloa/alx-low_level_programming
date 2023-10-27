73c80208b2be4b48f8264e945384c7eee337a504
Omo <omoikheloa56@gmail.com>
2023-10-27 09:23:56 -0700

    returns arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,16 +5,14 @@
  * @argc: number of command line arguments
  * @argv: array that contains the program command line arguments
  *
- * Return: 0 -success
+ * Return: 0 - success
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
-	}
 	return (0);
 }

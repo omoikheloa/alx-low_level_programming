14ef1c173bdcb51f35a0cdb25ce96836b1070888
Omo <omoikheloa56@gmail.com>
2023-11-06 15:10:04 -0800

    correction to header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,9 @@ typedef struct dog dog_t;
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name
+ * its age and the name of the owner
  */
 
 struct dog

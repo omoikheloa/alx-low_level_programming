7bc3d4b472967db75867ff72031960614c918611
Omo <omoikheloa56@gmail.com>
2023-11-09 05:53:57 -0800

    betty update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (j = 0; j < n; j++)
 	{
 		str = va_arg(args, char *);
-		
+
 		if (!str)
 			str = "(nil)";
 		if (!separator)

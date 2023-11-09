e62ad9d4b84cb2db0b82cff9fe01d87bc96edc68
Omo <omoikheloa56@gmail.com>
2023-11-09 05:49:58 -0800

    print strings correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			str = "(nil)";
 		if (!separator)
 			printf("%s", str);
-		else if (separator && i == 0)
+		else if (separator && j == 0)
 			printf("%s", str);
 		else
 			printf("%s%s", separator, str);

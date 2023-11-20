742bb246c085dc580f40bb750d517a2300918848
Omo <omoikheloa56@gmail.com>
2023-11-20 05:17:09 -0800

    test one again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t omo;
 
-	count = 0;
+	omo = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		count++;
+		omo++;
 	}
-	return (count);
+	return (omo);
 }

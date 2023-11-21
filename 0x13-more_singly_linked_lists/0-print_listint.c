a95834f02757bb312fb2e6d07f1cbe2cf547f2dc
Omo <omoikheloa56@gmail.com>
2023-11-21 09:40:16 -0800

    code update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t node_c = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		node_c++;
 	}
-	return (nnodes);
+	return (node_c);
 }

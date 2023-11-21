ce757718428a9218f9d8bb74915e6f957150a383
Omo <omoikheloa56@gmail.com>
2023-11-21 09:06:59 -0800

    freelists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
-	listint_t *temp;
 
 	if (head != NULL)
 	{
-		current = *head;
-		while (current != NULL)
+		while (*head != NULL)
 		{
-			current = current->next;
-			free(temp);
+			current = *head;
+			*head = (*head)->next;
+			free(current);
 		}
-		*head = NULL;
 	}
 }

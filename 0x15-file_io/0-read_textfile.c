446bb187d10497114c3fe499371c4560da3b58eb
Omo <omoikheloa56@gmail.com>
2023-11-27 10:35:15 -0800

    changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,38 +11,28 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t file_descriptor, bytes_read, bytes_written;
+	ssize_t open_file, bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(file_descriptor);
 		return (0);
-	}
 
-	bytes_read = read(file_descriptor, buffer, letters);
-
-	if (bytes_read == -1 || bytes_read != (ssize_t)letters)
-	{
-		free(buffer);
-		close(file_descriptor);
-		return (0);
-	}
-
+	open_file = open(filename, O_RDONLY);
+	bytes_read = read(open_file, buffer, letters);
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	free(buffer);
-	close(file_descriptor);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
+	if (bytes_read == -1 || bytes_written == -1 || open_file == -1
+		       || bytes_written != bytes_read)	
+	{
+		free(buffer);
 		return (0);
+	}
+
+	free(buffer);
+	close(open_file);
 
 	return (bytes_written);
 }
